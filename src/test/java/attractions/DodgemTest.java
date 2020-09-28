9fb3e46b57123d90b94664653beccac7373ccb8b
Timothy Saudners <timsaunders88@gmail.com>
2020-09-28 16:16:59 +0100

    added defaultPrice and priceFor methods to Dodgems class. tested

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor youngVisitor;
+    Visitor oldVisitor;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        youngVisitor = new Visitor(11, 100.00, 10.00);
+        oldVisitor = new Visitor(12, 201.00, 10.00);
     }
 
 
@@ -28,5 +33,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(4.50, this.dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void priceIsFullFor12YearOld(){
+        assertEquals(2.25, this.dodgems.priceFor(youngVisitor), 0.01);
+    }
+
+    @Test
+    public void priceIsHalfFor11YearOld(){
+        assertEquals(4.50, this.dodgems.priceFor(oldVisitor), 0.01);
     }
 }
