3870c12b8ea92c6bcca008f2db73e54231d7e9ca
Timothy Saudners <timsaunders88@gmail.com>
2020-09-28 16:24:16 +0100

    added defaultPrice and priceFor methods to RollerCoaster class. tested

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,19 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class RollerCoaster extends Attraction implements ITicketed {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    public double defaultPrice(){
+        return 8.50;
+    };
+
+    public double priceFor(Visitor visitor){
+        return visitor.getHeight() > 200.00 ? this.defaultPrice()*2 : this.defaultPrice();
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor shortVisitor;
+    Visitor tallVisitor;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        shortVisitor = new Visitor(11, 199.99, 10.00);
+        tallVisitor = new Visitor(12, 200.01, 10.00);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void priceIsDefaultForOneNineNinePointNineNine(){
+        assertEquals(8.50, this.rollerCoaster.priceFor(shortVisitor), 0.01);
+    }
+
+    @Test
+    public void priceIsDoubleForTwoZeroZeroPointZeroOne(){
+        assertEquals(17.00, this.rollerCoaster.priceFor(tallVisitor), 0.01);
     }
 }
